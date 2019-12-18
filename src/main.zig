3da05a86666dd34590f1841711e6c18fa75ce72d
Rickard Andersson <gonz@severnatazvezda.com>
2019-12-18 18:19:44 +0200

    update to work with latest zig

--- src/main.zig
+++ src/main.zig
@@ -17,19 +17,6 @@ pub fn main() anyerror!void {
     const seed = mem.readIntSliceLittle(u64, buf[0..8]);
     var allocator = &heap.ArenaAllocator.init(heap.direct_allocator).allocator;
 
-    // const context = spng.c.newContext().?;
-    // var buffer = try allocator.alloc(u8, 4096);
-    // _ = spng.c.setPngBuffer(context, buffer);
-    // spng.c.freeContext(context);
-    // var image_size: usize = undefined;
-    // _ = spng.c.decodedImageSize(context, @intToEnum(spng.c.Format, 1), &image_size);
-    // _ = spng.c.decodeImage(
-    //     context,
-    //     buffer,
-    //     spng.c.Format.RGBA16,
-    //     spng.c.DecodeFlags{},
-    // );
-
     var r = &DefaultPrng.init(seed).random;
 
     var p1 = Point2{ .x = -1, .y = 2 };
@@ -39,7 +26,6 @@ pub fn main() anyerror!void {
     var p5 = Point2{ .x = -1, .y = 0.5 };
     var points = ([_]Point2{ p1, p2, p3, p4, p5 })[0..];
 
-    // const centroids = try kmeans.kmeans(allocator, r, points, 5);
     const centroids = try kmeans.kmeans(
         allocator,
         r,
@@ -54,35 +40,9 @@ pub fn main() anyerror!void {
         2,
     );
     for (centroids) |c, i| {
-        warn("centroids[{}]: {}\n", i, c);
+        warn("centroids[{}]: {}\n", .{ i, c });
     }
     for (points) |p, i| {
-        warn("kmeans_points[{}]: {}\n", i, p);
+        warn("kmeans_points[{}]: {}\n", .{ i, p });
     }
-
-    // var p1 = Point3{ .x = -1, .y = 2, .z = 0.0 };
-    // var p2 = Point3{ .x = -1, .y = -1, .z = 0.0 };
-    // var p3 = Point3{ .x = 2, .y = 2, .z = 0.0 };
-    // var p4 = Point3{ .x = 2, .y = -1, .z = 0.0 };
-    // var p5 = Point3{ .x = -1, .y = 0.5, .z = 0.0 };
-    // var points = ([_]Point3{ p1, p2, p3, p4, p5 })[0..];
-    // // const centroids = try kmeans.kmeans(allocator, r, points, 5);
-    // const centroids = try kmeans.kmeans(
-    //     allocator,
-    //     r,
-    //     Point3,
-    //     points,
-    //     2,
-    //     kmeans.ClusteringInterface(Point3){
-    //         .distanceToFn = Point3.distanceTo,
-    //         .aggregateFn = Point3.aggregate,
-    //         .zero_element = Point3{ .x = 0.0, .y = 0.0, .z = 0.0 },
-    //     },
-    // );
-    // for (centroids) |c, i| {
-    //     warn("centroids[{}]: {}\n", i, c);
-    // }
-    // for (points) |p, i| {
-    //     warn("kmeans_points[{}]: {}\n", i, p);
-    // }
 }
